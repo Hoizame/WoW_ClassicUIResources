664ad3d6aef344fa3e67d60b031412da587b56cb
Lag <lagysera@gmail.com>
2019-10-08 19:30:57 +0200

    1.13.2.32089

--- RawData/Material.lua
+++ RawData/Material.lua
@@ -1,8 +1,8 @@
 -- This file is automatic generated and changes are not be reflected in future updates
 -- ID, Flags, FoleySoundID, SheatheSoundID, UnsheatheSoundID
 Material = {
-	{ 1, 1, 0, 0, 0 },
-	{ 2, 0, 0, 0, 0 },
+	{ 1, 1, 0, 698, 700 },
+	{ 2, 0, 0, 697, 699 },
 	{ 3, 0, 0, 0, 0 },
 	{ 4, 1, 0, 0, 0 },
 	{ 5, 5, 1005, 0, 0 },
---
+++ RawData/RibbonQuality.lua
@@ -0,0 +1,6 @@
+-- This file is automatic generated and changes are not be reflected in future updates
+-- ID, NumStrips, MaxSampleTimeDelta, AngleThreshold, MinDistancePerSlice, Flags
+RibbonQuality = {
+	{ 1, 1, 0.2, 0.5, 0.1, 0 },
+	{ 2, 1, 0.05, 0.9, 0.1, 0 },
+}
